b4959c850202b4968c8f6cce0d0925eae1424ab5
Esper77 <yo.moyo12355@gmail.com>
2023-02-24 23:46:23 +0300

    bugfix

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include "sorti.h"
 #include "easy_list.h"
-int main()
-{
+
+
+int main(){
     vector <int> mass;
     vector <int> mass2;
     int b;
     string a;
-    cin>>a;
+    cin >> a;
+
     while(a!="!"){
         for(int i=0; i<a.size(); i++){
             b=0;
@@ -15,19 +17,19 @@ int main()
         }
         cin>>a;
     }
-    int min;
-    min=list_min(mass);
-    while(mass2.size()<mass.size()){
-    while(mass[0]!=min){
-        rra(mass);
-        cout<<rra;
-    }
-        pb(mass, mass2);
-        cout<<pb;
-    }
-    for(int i1=0; i1<mass.size(); i1++){
-        pa(mass, mass2);
-        cout<<pa;
-    }
+
+    int minimum=list_min(mass);
+    while(mass2.size() < mass.size()){
+        while(mass[0] != minimum){
+            rra(mass);
+            cout<<"rra";
+        }
+            pb(mass, mass2);
+            cout<<"pb";
+        }
+        for(int i1=0; i1<mass.size(); i1++){
+            pa(mass, mass2);
+            cout<<"pa";
+        }
     return 0;
 }
