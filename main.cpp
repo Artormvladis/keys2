3a3761c32664ec462614730678229cc082292b2b
Esper77 <112155793+Esper77@users.noreply.github.com>
2023-02-25 13:15:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include "sorti.h"
 #include "easy_list.h"
-
-
-int main(){
+int main()
+{
     vector <int> mass;
     vector <int> mass2;
     int b;
     int i;
     string a;
-    cin >> a;
-
+    cin>>a;
     while(a!="!"){
         for(i=i+1; i<a.size(); i++){
             if(a[i]=" "){
@@ -20,22 +18,6 @@ int main(){
             mass.push_back(b-48);
         }
     }
-<<<<<<< HEAD
-
-    int minimum=list_min(mass);
-    while(mass2.size() < mass.size()){
-        while(mass[0] != minimum){
-            rra(mass);
-            cout<<"rra";
-        }
-            pb(mass, mass2);
-            cout<<"pb";
-        }
-        for(int i1=0; i1<mass.size(); i1++){
-            pa(mass, mass2);
-            cout<<"pa";
-        }
-=======
     int minn;
     minn=list_min(mass);
     while(mass2.size()<mass.size()){
@@ -50,6 +32,5 @@ int main(){
         pa(mass, mass2);
         cout<<pa;
     }
->>>>>>> a372f3d871f7c1656743def3043abe9bff61d503
     return 0;
 }
