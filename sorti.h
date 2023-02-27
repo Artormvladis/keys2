90f80e16e87f349feac280d7bb167fd1393d2ba2
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-27 13:32:19 +0300

    Update sorti.h

--- sorti.h
+++ sorti.h
@@ -4,8 +4,15 @@
 #include <string>
 #include <vector>
 using namespace std;
-void sa(vector <int> &mass);
+void sa(vector <int> &mass, vector <int> &mass2);
+void sb(vector <int> &mass, vector <int> &mass2);
+void ss(vector <int> &mass, vector <int> &mass2);
 void pb(vector <int> &mass, vector <int> &mass2);
 void pa(vector <int> &mass, vector <int> &mass2);
-void rra(vector <int> &mass);
+void rra(vector <int> &mass, vector <int> &mass2);
+void rrb(vector <int> &mass, vector <int> &mass2);
+void rrr(vector <int> &mass, vector <int> &mass2);
+void ra(vector <int> &mass, vector <int> &mass2);
+void rb(vector <int> &mass, vector <int> &mass2);
+void rr(vector <int> &mass, vector <int> &mass2);
 #endif // SORTI_H_INCLUDED
