1aa07a1e1cec4b1d7fbeb61d93c6c35bf6e02fba
Artormvladis <112126702+Artormvladis@users.noreply.github.com>
2023-02-27 13:50:45 +0300

    Add files via upload

---
+++ check_sortic.cpp
@@ -0,0 +1,58 @@
+#include"sorti.h"
+#include"easy_list.h"
+int main(){
+    vector <int> mass;
+    vector <int> mass2;
+    string fun;
+    int b = 0;
+    string a;
+    getline(cin, a);
+    for (int x = 0; a[x] != '\0'; x++){
+        if(a[x] == ' '){
+            mass.push_back(b);
+            b = 0;
+        }
+        else
+            b = b * 10 + (a[x] - 48);
+    }
+    mass.push_back(b);
+    cin>>fun;
+    while(fun!='!'){
+        if(fun=="sa"){
+           sa(mass, mass2);
+        }
+        if(fun=="sb"){
+           sb(mass, mass2);
+        }
+        if(fun=="ss"){
+           ss(mass, mass2);
+        }
+        if(fun=="pa"){
+           pa(mass, mass2);
+        }
+        if(fun=="pb"){
+           pb(mass, mass2);
+        }
+        if(fun=="ra"){
+           ra(mass, mass2);
+        }
+        if(fun=="rb"){
+           rb(mass, mass2);
+        }
+        if(fun=="rr"){
+           rr(mass, mass2);
+        }
+        if(fun=="rra"){
+           rra(mass, mass2);
+        }
+        if(fun=="rrb"){
+           rrb(mass, mass2);
+        }
+        if(fun=="rrr"){
+           rrr(mass, mass2);
+        }
+        cin>>fun;
+    }
+
+return 0;
+}
